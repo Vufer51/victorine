afdcdc8f820a260f10ee9bad5883f387365e06df
Vufer51 <kpot.hopa@gmail.com>
2021-11-22 00:03:50 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+
+  <b-row align-v="center" no-gutters align-h="center"  class="vh-100">
+    <b-col cols="auto">
+      <b-button size="lg" to="question">Го</b-button>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/question.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-row align-v="center" no-gutters align-h="center"  class="vh-100">
+    <b-col>
+      <b-row>
+        <b-col class="text-center">
+          <h3>Вопрос</h3>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-button @click="loadQuestions">Загрузить</b-button>
+        <b-col>{{quests}}</b-col>
+      </b-row>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: "question",
+  data: function () {
+    return {
+      quests:[],
+    }
+  },
+
+  mounted() {
+    this.loadQuestions()
+  },
+  methods: {
+    async loadQuestions(){
+      this.quests = (await this.$axios.get('questions.json')).data
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
